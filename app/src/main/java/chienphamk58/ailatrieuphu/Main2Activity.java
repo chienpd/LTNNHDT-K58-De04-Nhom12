f0e060f5f4201811a181f3c90ce2f51b285e74f9
chienphamk58 <anderson14196@gmail.com>
2016-04-13 17:36:07 +0700

    CountDownTimer :) It's very simple :D BackGround Music :P

--- app/src/main/java/chienphamk58/ailatrieuphu/Main2Activity.java
+++ app/src/main/java/chienphamk58/ailatrieuphu/Main2Activity.java
@@ -3,11 +3,16 @@ package chienphamk58.ailatrieuphu;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main2Activity extends AppCompatActivity {
     Intent intent;
+    CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +20,27 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         intent = new Intent(Main2Activity.this, PlaySongServiceLevel1.class);
         startService(intent);
+        final TextView myCounter = (TextView)findViewById(R.id.textView19);
+        countDownTimer = new CountDownTimer(600000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                myCounter.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)-TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            @Override
+            public void onFinish() {
+                myCounter.setText("TimeUp!");
+            }
+        }.start();
+
     }
 
     @Override
     protected void onStop(){
         super.onStop();
         stopService(intent);
+        countDownTimer.onFinish();
     }
 
 
@@ -28,5 +48,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         stopService(intent);
+        countDownTimer.onFinish();
     }
 }
--- app/src/main/java/chienphamk58/ailatrieuphu/Main3Activity.java
+++ app/src/main/java/chienphamk58/ailatrieuphu/Main3Activity.java
@@ -1,5 +1,6 @@
 package chienphamk58.ailatrieuphu;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,11 +8,14 @@ import android.view.WindowManager;
 
 public class Main3Activity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main3);
+        intent = new Intent(Main3Activity.this, PlaySongServiceLevel1.class);
+        startService(intent);
         mediaPlayer = new MediaPlayer().create(this, R.raw.luatchoi);
         mediaPlayer.start();
     }
@@ -20,6 +24,7 @@ public class Main3Activity extends AppCompatActivity {
     protected void onStop(){
         super.onStop();
         mediaPlayer.stop();
+        stopService(intent);
 
     }
 
@@ -27,5 +32,6 @@ public class Main3Activity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         mediaPlayer.stop();
+        stopService(intent);
     }
 }
